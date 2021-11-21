625fa31de2dfa4dc4eadd26043cb9c4e9e871d82
AllanFelipeF <allan_felipe1@live.com>
2021-11-21 11:00:40 -0300

    Aulas feitas, e exemplos da formação

---
+++ JS/testes.js
@@ -0,0 +1,28 @@
+//function ganhoPorHora(salario, horasTrabalhadasNoMes) {
+//
+//  const salarioHora = (salario / horasTrabalhadasNoMes); 
+//  
+//  const formatado = salarioHora.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+//  
+//  return formatado;
+//  
+//}
+//  
+//console.log(ganhoPorHora(3000,144));
+
+
+//var lista = [10,1, 5, 9, 8, 12, 15];
+//
+//function comparaNumeros(a,b) { if (a == b) return 0; if (a < b) return -1; if (a > b) return 1; }
+//console.log(lista.sort(comparaNumeros)); // [1, 5, 8, 9, 10, 12, 15]
+
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
---
+++ JS/tipos_primitivos.js
@@ -0,0 +1,40 @@
+//tipos primitivos
+
+//const num = 1;
+//
+//const primeiroNumero = 1;
+//
+//const segundoNumero = 2;
+//
+//const operacaoMatematica = primeiroNumero + segundoNumero;
+//
+//
+////Ponto flutuante
+//
+//
+//const num3 = .5;
+//const num4 = 3.3;
+//
+//const op = num3 * num4;
+//
+////console.log(op);
+//
+////Strings
+//const texto = 'batata triangular avermelhada';
+//
+//const texto2 = 'Vermelho batatal triangular';
+//
+//const senha = 'thisIsNotSafe231!@'
+//
+//const citacao = 'Olá "mundo"';
+//
+//console.log(citacao);
+
+
+//boolean
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero);
+
