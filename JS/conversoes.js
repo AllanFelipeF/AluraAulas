8bb1bfe7b44dc7ca35c5562240710dec68683c4f
AllanFelipeF <allan_felipe1@live.com>
2021-11-21 13:44:38 -0300

    codigos var e conversões

---
+++ JS/conversoes.js
@@ -0,0 +1,10 @@
+//Conversão implícita
+
+const num1 = 1;
+const num2 = '1'
+
+console.log(num1 == num2);
+
+console.log(num1 + Number(num2));
+//Conversão explícita
+
---
+++ JS/variaveis.js
@@ -0,0 +1,43 @@
+//var
+
+//var base = 12;
+//var altura = 7;
+//
+//area = base * altura;
+//
+//console.log(area);
+//
+//var area;
+
+//let
+
+//let forma = 'retangulo'
+//let alt = 12;
+//let base = 7;
+//let area;
+//
+//area = base * alt;
+//if(forma === 'retangulo'){
+//    area = base * alt;
+//} else {
+//    area = (alt * base) / 2;
+//}
+//console.log(area);
+//
+//let area;
+
+
+//const 
+
+const forma = 'retangulo';
+const base = 12;
+const alt = 7;
+const area;
+
+if(forma === 'retangulo') {
+    area = alt * base;
+} else {
+    area = (alt * base) / 2;
+}
+
+console.log(area);
