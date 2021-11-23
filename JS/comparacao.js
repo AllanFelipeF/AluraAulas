9b57424b7b53abe43a201b50a8c03cc110e99237
AllanFelipeF <allan_felipe1@live.com>
2021-11-23 11:43:15 -0300

    operadores, templatestring e operador ternario

---
+++ JS/comparacao.js
@@ -0,0 +1,14 @@
+// == (comparação implicita)
+
+const numero = 5;
+const texto = '5';
+
+console.log(numero == texto);
+
+// === (comparação implicita)
+
+
+const num1 = 5;
+const txt1 = '5';
+
+console.log(num1 === text);
---
+++ JS/template_string.js
@@ -0,0 +1,8 @@
+const nome = 'Allan';
+const idade = 24;
+const cidadeDeNascimento = 'Campinas';
+//const apresentacao = 'meu nome é ' + nome + ', minha idade é ' + idade + ' e nasci na cidade de ' + cidadeDeNascimento;
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
+
